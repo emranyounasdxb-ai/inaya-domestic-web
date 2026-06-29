6ad9ba4d405cd0005cf4fa1944c77c2485e1155f
Emran Younas <emranyounasdxb@gmail.com>
2026-06-29 19:35:24 +0400

    Include country source service copy

--- lib/service-page-copy-all.ts
+++ lib/service-page-copy-all.ts
@@ -1,11 +1,13 @@
 import { servicePageCopies as baseServicePageCopies } from './service-page-copy';
 import { servicePageCopiesNext } from './service-page-copy-next';
 import { servicePageCopiesMore } from './service-page-copy-more';
+import { servicePageCopiesCountry } from './service-page-copy-countries';
 
 export type { CompareRow, Lang, Pair, Pricing, ServiceCopy } from './service-page-copy';
 
 export const servicePageCopies = {
   ...baseServicePageCopies,
   ...servicePageCopiesNext,
-  ...servicePageCopiesMore
+  ...servicePageCopiesMore,
+  ...servicePageCopiesCountry
 };
