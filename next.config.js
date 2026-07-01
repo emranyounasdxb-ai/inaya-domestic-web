72d827400da77c9daf59e49ceff2cc0e7498e25d
Emran Younas <emranyounasdxb@gmail.com>
2026-07-01 15:30:55 +0400

    Prepare static export

--- next.config.js
+++ next.config.js
@@ -2,29 +2,12 @@ const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' }
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/images/home/candidate-executive-nanny.webp',
-        destination: '/images/home/candidates/elite-candidate-nanny.webp'
-      },
-      {
-        source: '/images/home/candidate-private-chef.webp',
-        destination: '/images/home/candidates/elite-candidate-chef.webp'
-      },
-      {
-        source: '/images/home/candidate-house-manager.webp',
-        destination: '/images/home/candidates/elite-candidate-house-manager.webp'
-      }
-    ];
+    unoptimized: true
   }
 };
 
